9724ba9d38815e707b4334317cc069c6ba029594
Xavier Geerinck <xavier.geerinck@gmail.com>
2023-09-16 18:27:35 +0200

    chore: update demo agent (#10)
    * update demo agent
    
    * comments in example code
    
    ---------
    
    Co-authored-by: Hunter Park <hunter.park11235@gmail.com>

---
+++ agents/demo/perceptors.py
@@ -0,0 +1,24 @@
+from composabl import Perceptor
+
+
+class DeltaCounter():
+    def __init__(self):
+        self.key = "state1"
+        self.previous_value = None
+
+    def compute(self, obs):
+        if self.previous_value is None:
+            self.previous_value = obs[self.key]
+            return {"delta_counter": 0, "state2": 0}
+        
+        delta = obs["state1"] - self.previous_value
+        self.previous_value = obs["state1"]
+        return {"delta_counter": delta, "state2": 0}
+    
+    def filtered_observation_space(self, obs):
+        return ["state1"]
+
+
+delta_counter = Perceptor(["delta_counter", "state2"], DeltaCounter, "the change in the counter from the last two steps")
+
+perceptors = [delta_counter]
