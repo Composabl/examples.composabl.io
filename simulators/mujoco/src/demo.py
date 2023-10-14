1260aea9d6997657d84eb2b89e6c387301faef2d
Xavier Geerinck <xavier.geerinck@gmail.com>
2023-10-14 17:28:58 +0200

    wip
    Signed-off-by: Xavier Geerinck <xavier.geerinck@gmail.com>

--- simulators/mujoco/src/demo.py
+++ simulators/mujoco/src/demo.py
@@ -1,16 +1,23 @@
+import cv2
 from composabl_core.grpc.client import client
 
+# python3 -m mujoco.viewer
 
 def start():
     # Examples
     # * CartPole-v1
     # * Hopper-v3
     # should be Hopper-v3 soon
-    c = client.make("run-demo", "sim-demo", "Hopper-v3", "localhost:1337", {})
+    c = client.make("run-demo", "sim-demo", "Walker2D", "localhost:1337", {})
     c.init()
 
     print(c.observation_space_info())
     print(c.action_space_info())
+
+    # # Display the img
+    # print(img)
+    # cv2.imshow("Sheep", np.array(img))
+    # cv2.waitKey(0)
 
 if __name__ == "__main__":
     start()
