781d1af90a20fbe68617a9657b85cc5932bcdf2f
Octavio Santiago <36106530+octavio-santiago@users.noreply.github.com>
2023-10-28 12:52:34 +0200

    DRL Multi programmed
    agent
    inference
    benchmaks

---
+++ agents/cstr/multiple_learned_skills_programmed/agent_inference.py
@@ -0,0 +1,190 @@
+import os
+
+from composabl import Agent, Runtime, Scenario, Sensor, Skill
+
+from teacher import CSTRTeacher, SS1Teacher, SS2Teacher, TransitionTeacher
+
+from cstr.external_sim.sim import CSTREnv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+license_key = os.environ["COMPOSABL_KEY"]
+
+from composabl import Controller
+
+class ProgrammedSelector(Controller):
+    def __init__(self):
+        self.counter = 0
+
+    def compute_action(self, obs):
+        if self.counter < 22:
+            action = [0]
+        elif self.counter < 74 : #transition
+            action = [1]
+        else:
+            action = [2]
+
+        self.counter += 1
+
+        return action
+
+    def transform_obs(self, obs):
+        return obs
+
+    def filtered_observation_space(self):
+        return ['T', 'Tc', 'Ca', 'Cref', 'Tref']
+
+    def compute_success_criteria(self, transformed_obs, action):
+        if self.counter > 100:
+            return True
+
+    def compute_termination(self, transformed_obs, action):
+        return False
+
+
+
+def start():
+    # delete old history files
+    dir = './cstr/multiple_learned_skills_programmed'
+    files = os.listdir(dir)
+    pkl_files = [file for file in files if file.endswith('inference_data.pkl')]
+    for file in pkl_files:
+        file_path = os.path.join(dir, file)
+        os.remove(file_path)
+
+    T = Sensor("T", "")
+    Tc = Sensor("Tc", "")
+    Ca = Sensor("Ca", "")
+    Cref = Sensor("Cref", "")
+    Tref = Sensor("Tref", "")
+
+    sensors = [T, Tc, Ca, Cref, Tref]
+
+    # Cref_signal is a configuration variable for Concentration and Temperature setpoints
+    ss1_scenarios = [
+        {
+            "Cref_signal": "ss1"
+        }
+    ]
+
+    ss2_scenarios = [
+        {
+            "Cref_signal": "ss2"
+        }
+    ]
+
+    transition_scenarios = [
+        {
+            "Cref_signal": "transition"
+        }
+    ]
+
+    selector_scenarios = [
+        {
+            "Cref_signal": "complete"
+        }
+    ]
+
+    ss1_skill = Skill("ss1", SS1Teacher, trainable=True)
+    for scenario_dict in ss1_scenarios:
+        ss1_skill.add_scenario(Scenario(scenario_dict))
+
+    ss2_skill = Skill("ss2", SS2Teacher, trainable=True)
+    for scenario_dict in ss2_scenarios:
+        ss2_skill.add_scenario(Scenario(scenario_dict))
+
+    transition_skill = Skill("transition", TransitionTeacher, trainable=True)
+    for scenario_dict in transition_scenarios:
+        transition_skill.add_scenario(Scenario(scenario_dict))
+
+    selector_skill = Skill("selector", ProgrammedSelector, trainable=False)
+    for scenario_dict in selector_scenarios:
+        selector_skill.add_scenario(Scenario(scenario_dict))
+
+    config = {
+        "license": license_key,
+        "target": {
+            "docker": {
+                "image": "composabl/sim-cstr:latest"
+            },
+            #"local": {
+            #    "address": "localhost:1337"
+            #}
+        },
+        "env": {
+            "name": "sim-cstr",
+        },
+        "runtime": {
+            "ray": {
+                "workers": 1
+            }
+        }
+    }
+
+    runtime = Runtime(config)
+    agent = Agent(runtime, config)
+    agent.add_sensors(sensors)
+
+    agent.add_skill(ss1_skill)
+    agent.add_skill(ss2_skill)
+    agent.add_skill(transition_skill)
+    agent.add_selector_skill(selector_skill, [ss2_skill, transition_skill, ss1_skill], fixed_order=False, fixed_order_repeat=False)
+
+    checkpoint_path = './cstr/multiple_learned_skills_programmed/saved_agents/'
+
+    #load agent
+    agent.load(checkpoint_path)
+
+    #save agent
+    trained_agent = agent.prepare()
+
+    # Inference
+    noise = 0.05
+    sim = CSTREnv()
+    sim.scenario = Scenario({
+            "Cref_signal": "complete",
+            "noise_percentage": noise
+        })
+    df = pd.DataFrame()
+    obs, info= sim.reset()
+    for i in range(90):
+        action = trained_agent.execute(obs)
+        #action = np.array((action[0]+10)/20)
+        obs, reward, done, truncated, info = sim.step(action)
+        df_temp = pd.DataFrame(columns=['T','Tc','Ca','Cref','Tref','time'],data=[list(obs) + [i]])
+        df = pd.concat([df, df_temp])
+
+        if done:
+            break
+
+    # save inference data
+    df.to_pickle("./cstr/multiple_learned_skills_programmed/inference_data.pkl")
+
+    # plot
+    plt.figure(figsize=(10,5))
+    plt.subplot(3,1,1)
+    plt.plot(df.reset_index()['time'],df.reset_index()['Tc'])
+    plt.ylabel('Tc')
+    plt.legend(['reward'],loc='best')
+    plt.title('Agent Inference Multi Skill Program Selector' + f" - Noise: {noise}")
+
+    plt.subplot(3,1,2)
+    #plt.plot(self.rms_history, 'r.-')
+    plt.plot(df.reset_index()['time'],df.reset_index()['T'])
+    plt.plot(df.reset_index()['time'],df.reset_index()['Tref'],'r--')
+    plt.ylabel('Temp')
+    plt.legend(['T', 'Tref'],loc='best')
+
+    plt.subplot(3,1,3)
+    plt.plot(df.reset_index()['time'],df.reset_index()['Ca'])
+    plt.plot(df.reset_index()['time'],df.reset_index()['Cref'],'r--')
+    plt.legend(['Ca', 'Cref'],loc='best')
+    plt.ylabel('Concentration')
+    plt.xlabel('iteration')
+
+    plt.savefig('./cstr/multiple_learned_skills_programmed/inference_figure.png')
+
+
+if __name__ == "__main__":
+    start()
