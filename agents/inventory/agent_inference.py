67b8e82679e0c6376faa8a0d36a83157f926b09e
Octavio Santiago <36106530+octavio-santiago@users.noreply.github.com>
2023-12-27 14:39:08 -0300

    Inference script update
    Inference script update

--- agents/inventory/agent_inference.py
+++ agents/inventory/agent_inference.py
@@ -84,7 +84,7 @@ def start():
     for i in range(30):
         action = trained_agent.execute(obs)
         action = action[0]
-        action = [action*100 for i in action]
+        action = [action*1 for i in action]
         obs, reward, done, truncated, info = sim.step(action[0])
 
         df_temp = pd.DataFrame(columns=['inventory','balance','num_ordered','order_cutoff','order_target','time'],
@@ -120,66 +120,10 @@ def start():
     plt.xlabel("Simulation time (days)")
 
 
-    print(f"Final Balance {df['balance'].to_list()[-1]}" )
+    print(f"Final Balance {df['balance'].to_list()[-1]} Total Balance {df['balance'].sum()}" )
 
 
     plt.savefig(f"{PATH}/inference_figure.png")
-
-
-
-
-    #random agent benchmark
-    sim = Env()
-    sim.scenario = Scenario({
-            "holding_cost": 2,
-            "cost_price": 20,
-            "delay_days_until_delivery": 5,
-            "customer_demand_min": 1,
-            "customer_demand_max": 3,
-            "selling_price": 25,
-            "run_time": 30
-        })
-    df = pd.DataFrame()
-    obs, info = sim.reset()
-    for i in range(30):
-        action = [random.randint(0,100),random.randint(0,100)]
-        obs, reward, done, truncated, info = sim.step(action)
-
-        df_temp = pd.DataFrame(columns=['inventory','balance','num_ordered','order_cutoff','order_target','time'],
-        data=[[obs[0],obs[1],obs[2],action[0], action[0],i]])
-        df = pd.concat([df, df_temp])
-
-        if done:
-            break
-
-    # plot
-    plt.figure(figsize=(10,5))
-    plt.subplot(4,1,1)
-    plt.plot(df.reset_index()['time'],df.reset_index()['inventory'])
-    plt.ylabel("Inventory level")
-    plt.title(f"Final Balance (Random Agent) {round(df['balance'].to_list()[-1],1)} $" )
-
-    plt.subplot(4,1,2)
-    plt.plot(df.reset_index()['time'],df.reset_index()['balance'])
-    plt.ylabel("Balance History ($)")
-
-
-    plt.subplot(4,1,3)
-    plt.plot(df.reset_index()['time'],df.reset_index()['order_cutoff'])
-    plt.ylabel('order_cutoff')
-
-
-    plt.subplot(4,1,4)
-    plt.plot(df.reset_index()['time'],df.reset_index()['order_target'])
-    plt.ylabel('order_target')
-    plt.xlabel("Simulation time (days)")
-
-
-    print(f"Final Balance (Random Agent) {df['balance'].to_list()[-1]}" )
-
-
-    plt.savefig(f"{PATH}/inference_figure_random_agent.png")
-
 
 if __name__ == "__main__":
     start()
