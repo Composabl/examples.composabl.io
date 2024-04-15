22fde1281b9ab3ecb8ce05380d7d4c1737391797
Kence Anderson <kence@composabl.ai>
2024-04-15 16:50:56 -0700

    Update sensors.py to add descriptions
    Signed-off-by: Kence Anderson <kence@composabl.ai>

--- 1_try/rocket_landing/agents/sensors.py
+++ 1_try/rocket_landing/agents/sensors.py
@@ -1,10 +1,10 @@
 from composabl import Sensor
 
-x = Sensor("x", "")
-x_speed = Sensor("x_speed", "")
-y = Sensor("y", "")
-y_speed = Sensor("y_speed", "")
-angle = Sensor("angle", "")
-ang_speed = Sensor("ang_speed", "")
+x = Sensor("x", "this is the x position of the starship")
+x_speed = Sensor("x_speed", "this is the speed in the x direction that the starship is headed")
+y = Sensor("y", "this is the y position of the starship")
+y_speed = Sensor("y_speed", "this is the speed in the y direction that the starship is headed")
+angle = Sensor("angle", "this is the angle of the starship")
+ang_speed = Sensor("ang_speed", "this is rate that the angle is changing")
 
 sensors = [x, x_speed, y, y_speed, angle, ang_speed]
