e6e3ffc9a2ba9c96fb31e3ddf073df6682284fb1
Gabriel Gomes <ribeiroggabriel@gmail.com>
2024-05-03 16:53:16 -0300

    Making `config.py` compatible with new `trainer` keywording

--- examples/aerial_autonomy/agents/config.py
+++ examples/aerial_autonomy/agents/config.py
@@ -5,18 +5,18 @@ license_key = os.environ["COMPOSABL_LICENSE"]
 config = {
         "license": license_key,
         "target": {
-            "docker": {
-                "image": "composabl/sim-starship"
-            },
-            #"local": {
-            #   "address": "localhost:1337"
-            #}
+            # "docker": {
+            #     "image": "composabl/sim-starship"
+            # },
+            "local": {
+              "address": "localhost:1337"
+            }
         },
         "env": {
             "name": "starship",
         },
         "training": {},
-        "runtime": {
+        "trainer": {
             "workers": 1
         }
     }
