1a9e5e057adc1344c93f941533e08c6c5fa0492b
Octavio Santiago <36106530+octavio-santiago@users.noreply.github.com>
2024-07-26 15:39:26 -0300

    CSTR sim update and bug fix
    CSTR sim update and bug fix

--- 2_learn/chemical_process_control/sim/src/sim.py
+++ 2_learn/chemical_process_control/sim/src/sim.py
@@ -11,39 +11,43 @@ from cstr_sim import cstr_model as cstr
 
 class CSTREnv(gym.Env):
     def __init__(self, *args, **kwargs):
-        '''
+        """
         actions = 1 : dTc (delta coolant temperature)
-        observation_variables = 5 :
+        sensor_variables = 5 :
             T (Temperature - K)
             Tc (Coolant temperature - K)
             Ca (Product Concentration (A) - kmol/m3)
             Cref (Product Concentration Reference (SetPoint)  - kmol/m3 )
             Tref (Temperature Reference (SetPoint) - K)
+            Concentratio_Error (Ca - Cref)
+            Episode Yield Percentage (% of time that the product is in spec)
+            Production Cb Total (Total of Cb produced)
         Cref_signal:
             1 - transition
             2 - ss1 - steady state 1 only
             3 - ss2 - steady state 2 only
             4 - complete
-        '''
+        """
         self.Cref_signal = "complete"
         self.noise_percentage = 0
-        self.scenario: Scenario = None
+        self.count = 0
+        self.T = 0
+        self.concentration_error = 0
+        self.tolerance = 0
+        self.episode_yield_pct = 0
+        self.production_Cb_total = 0
+        self.obs_history = []
+        self.y_list = []
+        self.error_list = []
+        self.scenario = None
 
-        self.observation_space = gym.spaces.Box(low=np.array([200, 200, 0, 0, 200]), high=np.array([500, 500, 12, 12, 500]))
-
+        self.observation_space = gym.spaces.Box(
+            low=np.array([200, 200, 0, 0, 200, 0, 0, 0]), high=np.array([500, 500, 12, 12, 500, 100, 1, 1000])
+        )
         self.action_space = gym.spaces.Box(low=np.array([-10.0]), high=np.array([10.0]))
 
 
     def reset(self):
-        # Define scenario in the simulation
-        if isinstance(self.scenario, Scenario):
-            sample = self.scenario.sample()
-
-            for key in list(sample.keys()):
-                setattr(self, key, sample[key])
-
-        noise_percentage = self.noise_percentage
-
         # initial conditions
         Ca0: float = 8.5698  # kmol/m3
         T0: float = 311.2639  # K
@@ -53,26 +57,49 @@ class CSTREnv(gym.Env):
         self.Tc = Tc0
         self.Ca = Ca0
         self.ΔTc = 0
+        self.tolerance = 0.5
+        self.Cref = 8.5698
+        self.Tref = 311.2612
+
+        # Define scenario in the simulation
+        if isinstance(self.scenario, Scenario):
+            sample = self.scenario.sample()
+
+            for key in list(sample.keys()):
+                setattr(self, key, sample[key])
+
+        noise_percentage = self.noise_percentage
+
         # time counter
-        self.cnt = 0
+        self.count = 0
 
         self.noise_percentage = noise_percentage
         # validation, if someone sends a noise not in the {0,1} format assume that they sent in pct values
         if self.noise_percentage > 1:
             self.noise_percentage = self.noise_percentage / 100
 
-        if self.Cref_signal == "ss1":
+        '''if self.Cref_signal == "ss1":
             self.Cref = 2
             self.Tref = 373.1311
         else:
             self.Cref = 8.5698
-            self.Tref = 311.2612
+            self.Tref = 311.2612'''
+
+        # Detect the Cref signal from scenarios
+        if self.Cref == 2 and self.Tref == 373.1311 and self.count == 0:
+            self.Cref_signal = "ss2"
+        elif self.Cref == 8.5698 and self.Tref == 311.2612 and self.count == 0:
+            self.Cref_signal = "ss1"
+        elif self.Cref == 8.5698 and self.Tref == 311.2612 and self.count > 1:
+            self.Cref_signal = "transition"
+        else:
+            self.Cref_signal = "complete"
 
         self.rms = 0
+        self.obs_history = []
         self.y_list = []
         self.error_list = []
-        self.obs = np.array([self.T, self.Tc, self.Ca, self.Cref, self.Tref])
-
+        self.obs = np.array([self.T, self.Tc, self.Ca, self.Cref, self.Tref, self.concentration_error, self.episode_yield_pct, self.production_Cb_total])
         info = {}
         return self.obs, info
 
@@ -81,14 +108,14 @@ class CSTREnv(gym.Env):
 
     def step(self, action):
         action = float(action[0])
-        if self.cnt >= 90:
-            self.cnt = 90
+        if self.count >= 90:
+            self.count = 90
         if self.Cref_signal == "transition":
             # update Cref an Tref
             time = 90
             p1 = 22
             p2 = 74
-            k = self.cnt + p1
+            k = self.count + p1
             C = interpolate.interp1d([0, p1, p2, time], [8.57, 8.57, 2, 2])
             self.Cref = float(C(k))
             T_ = interpolate.interp1d([0, p1, p2, time], [311.2612, 311.2612, 373.1311, 373.1311])
@@ -100,7 +127,7 @@ class CSTREnv(gym.Env):
             self.Cref = 8.5698
             self.Tref = 311.2612
         elif self.Cref_signal == "complete":
-            k = self.cnt
+            k = self.count
             time = 90
             # update Cref an Tref
             p1 = 22
@@ -135,13 +162,25 @@ class CSTREnv(gym.Env):
         self.Ca = sim_model.Ca + σ_Ca
         self.y_list.append(self.Ca)
 
+        self.obs_history.append([self.Ca,self.Cref])
+
         # Increase time counter
-        self.cnt += 1
+        self.count += 1
 
         # Error and Reward
         error = (self.Ca - self.Cref)**2
         self.error_list.append(error)
         self.rms = math.sqrt(sum(self.error_list) / len(self.error_list))
+
+        # instataneous yield regardless of time, it has to be in spec
+        yield_inst = 1 if abs(self.Ca - self.Cref) <= self.tolerance else 0
+        # yield percentage over the episode - % time that the product is in spec
+        self.episode_yield_pct = sum([ 1 if abs(x[0] - x[1]) <= self.tolerance else 0 for x in self.obs_history]) / len(self.obs_history)
+        #product needs to be in spec after 76 iterations to produce yield
+        product_Cb_inst = (10 - float(self.Ca)) if (self.count >= 76 and abs(self.Ca - self.Cref) <= self.tolerance) else 0
+        self.production_Cb_total = sum([ (10 - x[0]) if abs(x[0] - x[1]) <= self.tolerance else 0 for x in self.obs_history])
+
+        self.concentration_error = error
 
         #REWARD
         if self.T >= 400 :  # avoid
@@ -152,13 +191,12 @@ class CSTREnv(gym.Env):
         done = False
 
         # Constraints to break the simulation
-        if self.cnt == 90 or (self.Cref_signal == "transition" and self.cnt == 68):
+        if self.count == 90 or (self.Cref_signal == "transition" and self.count == 68):
             done = True
 
         info = {}
 
-        self.obs = np.array([self.T, self.Tc, self.Ca, self.Cref, self.Tref])
-
+        self.obs = np.array([self.T, self.Tc, self.Ca, self.Cref, self.Tref, self.concentration_error, self.episode_yield_pct, self.production_Cb_total])
         return self.obs, reward, done, False, info
 
     def render(self, mode='human', close=False):
