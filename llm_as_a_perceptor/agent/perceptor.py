6c1359c041b4db9ad7b36c07588a3763273a6da9
Hunter Park <hunter.park11235@gmail.com>
2024-09-17 16:18:01 -0500

    Update perceptor.py
    Signed-off-by: Hunter Park <hunter.park11235@gmail.com>

--- llm_as_a_perceptor/agent/perceptor.py
+++ llm_as_a_perceptor/agent/perceptor.py
@@ -1,3 +1,4 @@
+from fake_llm import llm_client
 from composabl_core import PerceptorImpl
 
 class TextPerceptor(PerceptorImpl):
@@ -5,13 +6,17 @@ class TextPerceptor(PerceptorImpl):
     The perceptor for the text agent
     """
     def __init__(self, *args, **kwargs):
+        # Example:
+        #self.llm_client = llm_client()
         pass
 
     async def compute(self, obs_spec, obs):
         """
         Reverses the string of the text sensor
         """
-        # print("Perceptor compute obs: ", obs)
+        # Example
+        # response = self.llm_client(f"You are controlling a CSTR plant, the current state of the plant is {obs}. what action do you recommend?")
+        # return {"chemical_engineer_llm": response}
         return {"text_reversed": obs["text"][::-1]}
     
     def filtered_sensor_space(self, obs):
