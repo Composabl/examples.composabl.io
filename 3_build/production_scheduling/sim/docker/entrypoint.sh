1fceaa2763e77994a0610de6ba0d8eb909f836d2
octaviobomfim <octaviobomfim@gmail.com>
2025-03-07 17:40:24 -0300

    Adapt Docker files to UI
    Adapt Docker files to UI

--- 3_build/production_scheduling/sim/docker/entrypoint.sh
+++ 3_build/production_scheduling/sim/docker/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+V_COMPOSABL_SIM=${V_COMPOSABL_SIM:-"1.1.0"}
+
 ######################################################
-# Composabl Sim Dockerfile - Version 1.0.0
+# Composabl Sim Entrypoint - Version 1.1.0
 ######################################################
 
 echo '===================================================================='
@@ -17,10 +19,10 @@ APP_CMD=${APP_CMD:-"python src/main.py"}
 APP_PATH=${APP_PATH:-"/app"}
 IS_DEBUG=${IS_DEBUG:-false}
 MODE=${MODE:-standalone}  # kubernetes | standalone
-IS_HISTORIAN_ENABLED=${IS_HISTORIAN_ENABLED:-"false"}
-COMPOSBAL_VERSION=$(python -c "import importlib.metadata; print(importlib.metadata.version('composabl-core'))" > /dev/null 2>&1)
+COMPOSABL_VERSION=$(python -c "import importlib.metadata; print(importlib.metadata.version('composabl-core'))" > /dev/null 2>&1)
+HISTORIAN_MONIKER_MQTT=${HISTORIAN_MONIKER_MQTT:-"tcp://mqtt:1883"}
 
-# Function to convert values to boolean
+# Function definitions
 to_boolean() {
     local value="$1"
     if [[ "$value" == "true" || "$value" == "True" || "$value" == "1" ]]; then
@@ -30,60 +32,46 @@ to_boolean() {
     fi
 }
 
+is_set() {
+    local value="$1"
+    if [[ -z "$value" ]]; then
+        echo "false"
+    else
+        echo "true"
+    fi
+}
+
 # Convert to boolean
-IS_HISTORIAN_ENABLED=$(to_boolean "$IS_HISTORIAN_ENABLED")
 IS_DEBUG=$(to_boolean "$IS_DEBUG")
+IS_HISTORIAN_ENABLED=$(is_set "$HISTORIAN_MONIKER_MQTT")
 
 # Generate SIM ID
 SIM_ID=${SIM_ID:-"sim-$(uuidgen)"}
 APP_NAME=$SIM_ID
-STANDALONE_MQTT_CONSUMER_ID=$SIM_ID
+HISTORIAN_MONIKER_MQTT_CONSUMER_ID=$SIM_ID
 
 # Print startup information
 echo "- SIM_ID: $SIM_ID"
 echo "- MODE: $MODE"
 echo "- IS_HISTORIAN_ENABLED: $IS_HISTORIAN_ENABLED"
-echo "- COMPOSABL_VERSION: $COMPOSBAL_VERSION"
+echo "- V_COMPOSABL_CORE: $(pip show "composabl-core-dev" | grep '^Version:' | awk '{print $2}')"
+echo "- V_COMPOSABL_SIM: $V_COMPOSABL_SIM"
 echo "- APP_NAME: $APP_NAME"
 echo "- APP_PATH: $APP_PATH"
 echo '===================================================================='
 
 if [[ "$IS_DEBUG" == "true" && "$MODE" == "standalone" ]]; then
-    echo "- STANDALONE_MQTT_URL: $STANDALONE_MQTT_URL"
-    echo "- STANDALONE_MQTT_CONSUMER_ID: $STANDALONE_MQTT_CONSUMER_ID"
+    echo "- HISTORIAN_MONIKER_MQTT: $HISTORIAN_MONIKER_MQTT"
+    echo "- HISTORIAN_MONIKER_MQTT_CONSUMER_ID: $HISTORIAN_MONIKER_MQTT_CONSUMER_ID"
     echo '===================================================================='
 fi
 
 # Navigate to the app directory
 cd "$APP_PATH"
 
-if [[ "$MODE" == "standalone" && "$IS_HISTORIAN_ENABLED" == "true" ]]; then
-    echo "[Dapr] Running with Dapr in Standalone mode, configuring components..."
+echo "Starting $APP_NAME"
+echo '===================================================================='
+HOST=${HOST:-"0.0.0.0"}
+PORT=${PORT:-"1337"}
 
-    # Patch /components/*yaml files with env variables
-    for file in /docker/dapr/components/*.yaml; do
-        echo "[Dapr] Patching $file with the env variables"
-
-        cp "$file" "$file.bak"
-        envsubst < "$file.bak" > "$file"
-        rm "$file.bak"
-    done
-
-    echo '===================================================================='
-
-    # Start the app with dapr
-    echo "[Dapr] Starting $APP_NAME with Dapr"
-    echo "[Dapr] - PATH: '$APP_PATH'"
-    echo "[Dapr] - CMD: '$APP_CMD'"
-    echo '===================================================================='
-
-    dapr run --app-id "$SIM_ID" --app-port 50051 --dapr-http-port 3500 --app-protocol grpc \
-        --resources-path /docker/dapr/components \
-        -- \
-        $APP_CMD
-else
-    echo "Starting $APP_NAME"
-    echo '===================================================================='
-
-    $APP_CMD
-fi
+$APP_CMD --host $HOST --port $PORT
